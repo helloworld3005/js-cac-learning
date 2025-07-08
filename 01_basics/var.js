bcd09a3ec7867064ffa18148d74eb9644a10625d
helloworld3005 <prathamsharma3005@gmail.com>
2025-07-08 12:52:56 +0000

    var

---
+++ 01_basics/var.js
@@ -0,0 +1,8 @@
+const accountID = 123562
+let accountName = "Pratham Sharma"
+accountPassword = "pratham007"
+
+// accountID = 23215256 // not allowed becoz we have used const 
+
+console.log(accountID)
+console.table([accountID , accountName , accountPassword])
